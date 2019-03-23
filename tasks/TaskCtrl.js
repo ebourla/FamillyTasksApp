105fc4487b2b41eefbb7ca44636ee575cb213f46
ebourla <elizabethbourla@gmail.com>
2019-03-23 18:03:41 +0200

    TaskCTRL chnages

--- tasks/TaskCtrl.js
+++ tasks/TaskCtrl.js
@@ -7,9 +7,9 @@ app.controller("TaskCtrl", function ($scope, taskSrv, userSrv, $location, $log) 
 
   $scope.activeUser = userSrv.getActiveUser();
 
-  $scope.markedItemArr = [];
-  $scope.currentFilterStatus = 0;
-  $scope.taskList = [];
+  // $scope.markedItemArr = [];
+  // $scope.currentFilterStatus = 0;
+  // $scope.taskList = [];
  
 
   // function Task(name, status) {
@@ -27,73 +27,77 @@ app.controller("TaskCtrl", function ($scope, taskSrv, userSrv, $location, $log) 
 
 
   //currentFilterStatus
-  $scope.onStatusChanged = function (displayStatus) {
-    //$scope.currentFilterStatus = displayStatus;
+  // $scope.onStatusChanged = function (displayStatus) {
+  //   //$scope.currentFilterStatus = displayStatus;
 
-    // loop on  $scope.taskList
-    // find all elemnts with displayStatus
+  //   // loop on  $scope.taskList
+  //   // find all elemnts with displayStatus
 
-    $scope.filteredList = [];
+  //   $scope.filteredList = [];
 
-    for (var i = 0; i < $scope.taskList.length; i++) {
-      var element = $scope.taskList[i];
+  //   for (var i = 0; i < $scope.taskList.length; i++) {
+  //     var element = $scope.taskList[i];
 
-      if (displayStatus === 0) {
-        // all
-        $scope.filteredList = $scope.taskList;
-      } else {
-        if (element.status === displayStatus) {
-          $scope.filteredList.push(element);
-        }
-      }
+  //     if (displayStatus === 0) {
+  //       // all
+  //       $scope.filteredList = $scope.taskList;
+  //     } else {
+  //       if (element.status === displayStatus) {
+  //         $scope.filteredList.push(element);
+  //       }
+  //     }
 
-    }
-  }
+  //   }
+  // }
 
-  $scope.taskAdd = function () {
-    if ($scope.taskList) {
-      $scope.addTaskToArr($scope.taskList, 1);
-    }
-    $scope.taskList = "";
-  };
+  // $scope.taskAdd = function () {
+  //   if ($scope.taskList) {
+  //     $scope.addTaskToArr($scope.taskList, 1);
+  //   }
+  //   $scope.taskList = 
+  // };
 
-  $scope.remove = function () {
-    if (window.confirm("Are you sure you want to delete?")) {
+  // $scope.remove = function () {
+  //   if (window.confirm("Are you sure you want to delete?")) {
 
-      for (var i = 0; i < $scope.markedItemArr.length; i++) {
-        var x = $scope.markedItemArr[i];
-        var pos = $scope.taskList.indexOf(x);
-        if (pos > -1) {
-          $scope.taskList.splice(pos, 1);
-        }
-      }
-    }
-  }
+  //     for (var i = 0; i < $scope.markedItemArr.length; i++) {
+  //       var x = $scope.markedItemArr[i];
+  //       var pos = $scope.taskList.indexOf(x);
+  //       if (pos > -1) {
+  //         $scope.taskList.splice(pos, 1);
+  //       }
+  //     }
+  //   }
+  // }
 
-  $scope.addTaskToArr = function (name, status) {
-    if (!$scope.taskList) {
-      $scope.taskList = [];
-    }
+  // $scope.addTaskToArr = function (name, status, description) {
+  //   if (!$scope.taskList) {
+  //     $scope.taskList = [];
+  //     $scope.filteredList = $scope.taskList;
+  //   }
 
-    // $scope.taskList = taskSrv.addTaskToArr($scope.taskList, name, status)
-    $scope.filteredList = $scope.taskList;
-  }
+    
+  // }
 
-  $scope.onCheckboxChecked = function (checkedItem) {
+  // $scope.onCheckboxChecked = function (checkedItem) {
 
-    if (checkedItem.Task.isCompleted) {
-      checkedItem.Task.status = 2;
-      $scope.markedItemArr.push(checkedItem.Task)
+  //   if (checkedItem.Task.isCompleted) {
+  //     checkedItem.Task.status = 2;
+  //     $scope.markedItemArr.push(checkedItem.Task)
 
-    } else {
-      checkedItem.Task.status = 0;
-      var pos = $scope.markedItemArr.indexOf(checkedItem);
-      if (pos > -1) {
-        $scope.markedItemArr.slice(pos, 1)
-      }
-    }
-    console.log(event)
-  }
+  //   } else {
+  //     checkedItem.Task.status = 0;
+  //     var pos = $scope.markedItemArr.indexOf(checkedItem);
+  //     if (pos > -1) {
+  //       $scope.markedItemArr.slice(pos, 1)
+  //     }
+  //   }
+  //   console.log(event)
+  // }
 
-   $scope.addTaskToArr('Clean House', 0);
+  //  $scope.addTaskToArr('Clean House', 0);
 });
+
+// function newFunction() {
+//   return "";
+// }
