2d070bb17e8ce6b33e0b78e504bc0724b4160c85
ebourla <elizabethbourla@gmail.com>
2019-03-24 00:28:11 +0200

    newTask problem

--- tasks/newTaskCtrl.js
+++ tasks/newTaskCtrl.js
@@ -1,9 +1,9 @@
-app.controller("newTaskCtrl", function($scope, tasksSrv, $location, $log) {
+app.controller("newTaskCtrl", function($scope, tasksSrv, userSrv, $location, $log) {
     
     $scope.createTask = function() {
         // call service createTask
         tasksSrv.createTask($scope.name, $scope.date, $scope.userID, $scope.isCompleted, $scope.description).then(function() {
-            $location.path("/Tasks");
+            $location.path("Tasks");
         }, function(err) {
             $log.error(err);
         })
