43d33842cf179b0c79305e0dc3b4363ac7db69a5
ebourla <elizabethbourla@gmail.com>
2019-03-24 10:29:46 +0200

    addTask

--- tasks/newTaskCtrl.js
+++ tasks/newTaskCtrl.js
@@ -3,7 +3,7 @@ app.controller("newTaskCtrl", function($scope, taskSrv, $location, $log) {
     $scope.createTask = function() {
         // call service createTask
         taskSrv.createTask($scope.name, $scope.date, $scope.userID, $scope.isCompleted, $scope.description).then(function() {
-            $location.path("Tasks/newTask");
+            $location.path("/tasks");
         }, function(err) {
             $log.error(err);
         })
